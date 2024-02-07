94b8bca6c1eb4322b9ce6d80fbd950c012b0130b
leesiyuan <lee.si.yuan.20@gmail.com>
2024-02-07 21:38:14 +0800

    Update Home Page

---
+++ src/components/eventComponents/TextComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+    msg: string
+}
+
+const HeaderComponent = (props: Props) => {
+  return (
+    <div className='p-8 w-3/4 flex h-[200px] border-2 rounded-xl'>
+      <p className='text-xl'>{props.msg}</p>
+    </div>
+  )
+}
+
+export default HeaderComponent
